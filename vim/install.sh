64b76da3d17ba50c8a4ca4ff304e12722040cfe5
Lucas Fontes <lxfontes@gmail.com>
2011-12-13 11:40:29 -0500

    ditched vundle.. bunch of git repositories are busted

--- vim/install.sh
+++ vim/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 D=`dirname $0`
-mkdir -p $HOME/.vim/bundle
 
 if [ -f $HOME/.vim/vimrc ]; then
   cp $HOME/.vim/vimrc $HOME/.vim/vimrc.zzbk
 fi
 cp $D/vimrc $HOME/.vim/vimrc
-if [ -d $HOME/.vim/vundle.git ]; then
-(cd $HOME/.vim/vundle.git && git pull)
-else
-git clone http://github.com/gmarik/vundle.git $HOME/.vim/vundle.git
-fi
+cp $D/update_bundles $HOME/.vim/update_bundles
+
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle; \
+	curl -so $HOME/.vim/autoload/pathogen.vim \
+	    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+
 cp -r $D/sandvine $HOME/.vim/bundle/
